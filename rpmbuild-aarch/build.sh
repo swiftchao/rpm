b1ab23fabd89d1891634a5a499300c5cab7c84b8
chaofei <chaofeibest@163.com>
2022-04-01 17:25:10 +0800

    【rpm编译】增加mock编译aarch平台的包和x86编译的修改

---
+++ rpmbuild-aarch/build.sh
@@ -0,0 +1,1 @@
+rpmbuild -ba '--define=_topdir /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild' '--define=_tmppath /home/mockbuild/tmp/rpmbuild' /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild/SPECS/hellorpm.spec
---
+++ rpmbuild-x86/build.sh
@@ -0,0 +1,1 @@
+rpmbuild -ba '--define=_topdir /home/mockbuild/rpm/rpmbuild-x86/rpmbuild' '--define=_tmppath /home/mockbuild/tmp/rpmbuild' /home/mockbuild/rpm/rpmbuild-x86/rpmbuild/SPECS/hellorpm.spec
