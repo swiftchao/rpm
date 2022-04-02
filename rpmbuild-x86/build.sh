7325a6bf2dbd015a3c8bae71e3723ea1ec95c149
chaofei <chaofeibest@163.com>
2022-04-02 14:43:26 +0800

    【rpm编译】更新x86编译脚本

--- rpmbuild-x86/build.sh
+++ rpmbuild-x86/build.sh
@@ -1,1 +1,3 @@
 rpmbuild -ba '--define=_topdir /home/mockbuild/rpm/rpmbuild-x86/rpmbuild' '--define=_tmppath /home/mockbuild/tmp/rpmbuild' /home/mockbuild/rpm/rpmbuild-x86/rpmbuild/SPECS/hellorpm.spec
+cp -rf /home/mockbuild/rpm/rpmbuild-x86/rpmbuild/RPMS/x86_64/*.rpm /home/mockbuild/rpm/result/x86/
+cp -rf /home/mockbuild/rpm/rpmbuild-x86/rpmbuild/SRPMS/*.rpm /home/mockbuild/rpm/result/src/
