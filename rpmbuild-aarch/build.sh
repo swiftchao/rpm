a30efa3cc238ca2d3ccdd451c7258541561d0727
chaofei <fchao@fiberhome.com>
2022-04-02 14:30:40 +0800

    【rpm编译】更新aarch包的编译

--- rpmbuild-aarch/build.sh
+++ rpmbuild-aarch/build.sh
@@ -1,1 +1,3 @@
 rpmbuild -ba '--define=_topdir /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild' '--define=_tmppath /home/mockbuild/tmp/rpmbuild' /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild/SPECS/hellorpm.spec
+cp -rf /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild/RPMS/aarch64/*.rpm /home/mockbuild/rpm/result/aarch/
+cp -rf /home/mockbuild/rpm/rpmbuild-aarch/rpmbuild/SRPMS/*.rpm /home/mockbuild/rpm/result/src/
